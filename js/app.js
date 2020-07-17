eec26a2ffc2df73a4eec1decef20ccffca404e80
larry <yourehilarryous@gmail.com>
2020-07-16 20:32:49 -0700

    Fully functioning room, randomization, and button functionality, CSS and fine details required

--- js/app.js
+++ js/app.js
@@ -13,14 +13,30 @@
     }
 
     const enemy = [    
-        { enemyCategorey: "slime", name: "Henry", enemyType: "Small", sorted: true, greeting: "\"I will consume you and you will NOT be daijob\"" },
-        { enemyCategorey: "Mutated Rat", name: "Gary", enemyType: "Small", sorted: true, greeting: "\"(Uncomforatable chittering)\"" }, 
-        { enemyCategorey: "Decaying Wolf", name: "Dread", enemyType: "Mobile",sorted: true,  greeting: "\"(Low blood-curdling growl)\"" }, 
-        { enemyCategorey: "Skeletal Solider", name: "Francis", enemyType: "Mobile", sorted: true, greeting: "\"(bones clattering)\""},
-        { enemyCategorey: "Stone Golem", name: "Hagrid", enemyType: "Stationary", sorted: true,greeting: "\"You're a wiz... You're not Harry!\"" }, 
-        { enemyCategorey: "Troll", name: "Za'thuk, the Flesh Renderer", enemyType: "Impassible", sorted: true, greeting: "\"Do you have something for me, mon?\""} // will need to set parameters for passing this enemy
+        { enemyCategorey: "slime", name: "Henry", enemyType: "Small",  greeting: "\"I will consume you and you will NOT be daijob\"" },
+        { enemyCategorey: "Mutated Rat", name: "Gary", enemyType: "Small",  greeting: "\"(Uncomforatable chittering)\"" }, 
+        { enemyCategorey: "Demon Bat", name: "Rick", enemyType: "Small", greeting: "\"No, I'm not a pickle\""},
+        { enemyCategorey: "Decaying Wolf", name: "Dread", enemyType: "Mobile",  greeting: "\"(Low blood-curdling growl)\"" }, 
+        { enemyCategorey: "Grave Robber", name: "Poncho", enemyType: "Mobile", greeting: "\"My phone died and now I can't click the next room function\""},
+        { enemyCategorey: "Skeletal Solider", name: "Francis", enemyType: "Mobile",  greeting: "\"(bones clattering)\""},
+        { enemyCategorey: "Stone Golem", name: "Hagrid", enemyType: "Stationary", greeting: "\"You're a wiz... You're not Harry!\"" }, 
+        { enemyCategorey: "Curse Armor", name: "Jarod", enemyType: "Stationary", greeting: "\"Dude stole my legs. Can you believe that?\""},
+        { enemyCategorey: "Troll", name: "Za'thuk, the Flesh Renderer", enemyType: "Impassible",  greeting: "\"Do you have something for me, mon?\""} // will need to set parameters for passing this enemy
     ]
 
+    //will need to create additional enemies 
+
+    let option1 = document.querySelector(".option1")
+    let option2 = document.querySelector(".option2")
+    const button1 = document.querySelector("#btn1")
+    const button2 = document.querySelector("#btn2")
+    let narrative = document.querySelector(".flavor-text p")
+    let mazeEnemy = ""
+    // let randomEnemy = enemy
+    let roomCount = 0
+    let roomButton1 = document.createElement("button")
+    let roombutton2 = document.createElement("button")
+    const end = new Audio("Media/death.mp3")
 
 // Functions
     // Welcome screen
@@ -46,53 +62,54 @@
                 // Set a conditional that will only spawn small and medium enemies in the first roomss 
                 // The second room will spawn a medium to large enemy.
 
-let option1 = document.querySelector(".option1")
-let option2 = document.querySelector(".option2")
-const button1 = document.querySelector("#btn1")
-const button2 = document.querySelector("#btn2")
-let narative = document.querySelector(".flavor-text p")
-let mazeEnemy = ""
-// let randomEnemy = enemy
-let gameCount = 0
+
+
 
 
 function checkGameDagger() {
-    if (gameCount === 0){
+    if (roomCount === 0){
         startMaze("Dagger")
-        console.log(gameCount)
-    } else if (gameCount === 1){
+        console.log(roomCount)
+    } else if (roomCount === 1){
         firstRoom()
-        console.log(gameCount)
-    } else if (gameCount === 2 && daggerPreReqs() === true) {
+        console.log(roomCount)
+    } else if (roomCount === 2 && daggerPreReqs()) {
         secondRoom()
-        console.log(gameCount)
-    } else if (gameCount === 3 && daggerPreReqs() === true){
-        //thirdRoom
+        console.log(roomCount)
+    } else if (roomCount === 3 && daggerPreReqs()){
+        thirdRoom()
+        console.log(roomCount)    
+    } else if (roomCount === 4 && daggerPreReqs()){
+        fourthRoom()
+        console.log(roomCount)    
     } else {
         gameOver()
         return
     }
-    gameCount++
-
+    roomCount++
 }
 
 function checkGameCloak() {
-    if (gameCount === 0){
+    if (roomCount === 0){
         startMaze("Cloak")
-        console.log(gameCount)
-    } else if (gameCount === 1){
+        console.log(roomCount)
+    } else if (roomCount === 1){
         firstRoom()
-        console.log(gameCount)
-    } else if (gameCount === 2 && cloakPreReqs === true) {
+        console.log(roomCount)
+    } else if (roomCount === 2 && cloakPreReqs()) {
         secondRoom()
-        console.log(gameCount)
-    } else if (gameCount === 3 && cloakPreReqs === true){
-        //thirdRoom
+        console.log(roomCount)
+    } else if (roomCount === 3 && cloakPreReqs()){
+        thirdRoom()
+        console.log(roomCount)
+    } else if (roomCount === 4 && cloakPreReqs()){
+        fourthRoom()
+        console.log(roomCount)    
     } else {
         gameOver()
         return
     }
-    gameCount++
+    roomCount++
 }
 
 
@@ -109,8 +126,8 @@ function startMaze (target) {
     console.log(player.utilityItem)
     
     document.querySelector("h2").classList.toggle("hidden")
-    narative.innerHTML = "24px" // does not work, will need to figure out how change the font size
-    narative.innerHTML = "So you chose the " + target + "? Not my first choice, but sure. Now for the fun part!"+ "<br />" + "<br />" + "I mean... It's time fo you to begin the challenge" + "<br />" + "<br />" + "(make sure to read this in a cool ominus voice)" 
+    narrative.innerHTML = "24px" // does not work, will need to figure out how change the font size
+    narrative.innerHTML = "So you chose the " + target + "? Not my first choice, but sure. Now for the fun part!"+ "<br />" + "<br />" + "I mean... It's time fo you to begin the challenge" + "<br />" + "<br />" + "(make sure to read this in a cool ominus voice)" 
 
     // Change both icons above the selection buttons
 
@@ -133,9 +150,6 @@ const randomize = () => {
         const randomE = enemy[j]
         enemy[j] = enemy[i]
         enemy[i] = randomE
-        if(randomE.sorted === true){
-            randomE.sorted = false 
-        }
     }
 }
 
@@ -147,9 +161,10 @@ function firstRoom () {
 
     mazeEnemy = enemy.shift()
     console.log(mazeEnemy)
+    console.log(player.utilityItem)
 
     document.querySelector("h2").classList.toggle("hidden")
-    narative.innerHTML = "Good job! Oh wait, looks like you ran into " + mazeEnemy.name + "!" + "<br />" + "<br />" + mazeEnemy.name + ": " + mazeEnemy.greeting
+    narrative.innerHTML = "Good job! Oh wait, looks like you ran into a " + mazeEnemy.enemyCategorey + "!" + "<br />" + "<br />" + mazeEnemy.name + ": " + mazeEnemy.greeting
 
     if (player.utilityItem === "Dagger"){
         daggerPreReqs()
@@ -177,13 +192,13 @@ function daggerPreReqs() {
 
 function cloakPreReqs(){
     if(mazeEnemy.enemyType === "Small") {
-        secondRoom();
+        return true
     } else if (mazeEnemy.enemyType === "Mobile"){
-        gameOver();
+        return false
     } else if (mazeEnemy.enemyType === "Stationary"){
-        secondRoom();
+        return true
     } else {
-        gameOver();
+        return false
     }
 }
 
@@ -197,8 +212,9 @@ function secondRoom () {
 
     mazeEnemy = enemy.shift()
     console.log(mazeEnemy)
+    console.log(player.utilityItem)
 
-    narative.innerHTML = "Well <strong> THAT </strong> was close! Looks likes you were able to get past him."
+    narrative.innerHTML = "Well <strong> THAT </strong> was close! Looks likes you were able to get past him."
 
     if (player.utilityItem === "Dagger"){
         daggerPreReqs()
@@ -207,9 +223,65 @@ function secondRoom () {
     }
 }
 
-function gameOver(){
+
+
+
+function thirdRoom (){ 
     document.querySelector("h2").classList.toggle("hidden")
-    narative.innerHTML = "game over"
+
+    randomize();
+
+    mazeEnemy = enemy.shift()
+    console.log(mazeEnemy)
+
+    narrative.innerHTML = "Looks like we got past another one, you're pretty good at this."
+
+    roomButton1.innerText = "Keep going"
+    document.querySelector("div.flavor-text").appendChild(roomButton1)
+    roomButton1.setAttribute("class", "confirmation")
+
+    roombutton2.innerText = "Lay Down and wait for certain doom"
+    document.querySelector("div.flavor-text").appendChild(roombutton2)
+    roombutton2.setAttribute("class", "confirmation")
+
+    roomButton1.onclick = function () {
+        document.querySelector(".confirmation-message").innerText = "Let's do this then!"
+
+        roomButton1.remove()
+        roombutton2.remove()
+    }
+    roombutton2.onclick = function () {
+        
+        gameOver()
+
+        roomButton1.remove()
+        roombutton2.remove()
+    }
+
+    if (player.utilityItem === "Dagger"){
+        daggerPreReqs()
+    } else if (player.utilityItem === "Cloak"){
+        cloakPreReqs()
+    }
+}
+
+function fourthRoom() { 
+
+    document.querySelector(".confirmation-message").innerText = ""
+    document.querySelector("h2").classList.toggle("hidden")
+
+    randomize();
+
+    mazeEnemy = enemy.shift()
+    console.log(mazeEnemy)
+
+    narrative.innerHTML = "Test"
+}
+
+function gameOver(){
+    end.play()
+    document.querySelector("h2").classList.toggle("hidden")
+    narrative.innerHTML = "Oof. Welp, can't say I didn't see that coming"
     console.log("still works!")
 }
 
